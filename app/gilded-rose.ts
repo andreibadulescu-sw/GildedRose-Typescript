2e1b0375e40b9905ebeb2e1cb3cc1916381fa2fc
Andrei-Marcel Badulescu <andrei-marcel.badulescu@softwire.com>
2026-07-09 13:04:12 +0300

    [test] Add full coverage Unit Tests for GildedRose
    Signed-off-by: Andrei-Marcel Badulescu <andrei-marcel.badulescu@softwire.com>

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,6 +17,10 @@ export class GildedRose {
         this.items = items;
     }
 
+    // progressTime()
+    // the sellIn value that an item enters the function
+    // with represents the value of the previous day, not
+    // the current day
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -7,24 +7,36 @@ describe('Gilded Rose Master Tests', function () {
     it('MT 1', function() {
         const gildedRose = new GildedRose([
             new Item('Sulfuras, Hand of Ragnaros', 100, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 20, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 9, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 7, 20),
             new Item('Backstage passes to a TAFKAL80ETC concert', 3, 20),
             new Item('Backstage passes to a TAFKAL80ETC concert', 1, 100),
             new Item('Aged Brie', 1, 45),
+            new Item('Aged Brie', 1, 50),
             new Item('Magic Crow', 14, 30),
             new Item('Cow Powder', 0, 25),
-            new Item('Conjured Tomato', 7, 10)
+            new Item('Atomic Pen', 20, 1),
+            new Item('Conjured Tomato', 7, 10),
+            new Item('Conjured Ketchup', 1, 10)
         ]);
         
         const items = new GildedRose(gildedRose.updateQuality()).updateQuality();
         
         expect(items).to.equal([
             new Item('Sulfuras, Hand of Ragnaros', 100, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 18, 22),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 7, 24),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 25),
             new Item('Backstage passes to a TAFKAL80ETC concert', 1, 26),
-            new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0),
-            new Item('Aged Brie', -1, 47),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0),
+            new Item('Aged Brie', 0, 48),
+            new Item('Aged Brie', 0, 50),
             new Item('Magic Crow', 12, 28),
-            new Item('Cow Powder', -2, 23),
-            new Item('Conjured Tomato', 5, 6)
+            new Item('Cow Powder', 0, 21),
+            new Item('Atomic Pen', 18, 0),
+            new Item('Conjured Tomato', 5, 6),
+            new Item('Conjured Ketchup', 0, 4)
         ]);
     });
     
